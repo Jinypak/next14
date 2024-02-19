034865cfa0703156de507b31aa519fb8b0851e93
park jinyong <jiny3360@nine2021.com>
2024-02-19 21:02:47 +0900

    20240219 : make sidebar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+
+type Props = {};
 
 const page = (props: Props) => {
+  const { user } = useUser();
   return (
-    <div>Protect Document Page</div>
-  )
-}
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2>Welcome to {user?.firstName}&apos;s Notion</h2>
+      <Button className="text-lg font-medium">
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -3,19 +3,30 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
+import React from "react";
 
-const MainLayout = ({ children }: {children: React.ReactNode}) => {
-    const { isAuthenticated, isLoading } = useConvexAuth();
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-    if(isLoading) {return (<div className="h-full flex items-center justify-center"><Spinner size="lg"/></div>);}
-
-    if(!isAuthenticated) {
-        return redirect("/");
-    }
-
+  if (isLoading) {
     return (
-        <div>{children}</div>
-    )
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full">{children}</main>
+    </div>
+  );
 };
 
 export default MainLayout;
